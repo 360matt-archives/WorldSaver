561dc3b6edf61b4d476f84a6b6403fa0ed2879d3
360matt <mattbacq2004@gmail.com>
2020-08-05 01:33:40 +0200

    finalisation du CallBack, avec cooldown pour des call espacés par temps

--- src/main/java/fr/ulity/worldsaver/api/Save.java
+++ src/main/java/fr/ulity/worldsaver/api/Save.java
@@ -6,6 +6,7 @@ import org.bukkit.*;
 
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -25,6 +26,7 @@ public abstract class Save {
         this.world = world;
         this.chunks = chunks;
         this.filename = filename;
+        this.totalChunks = chunks.length;
     }
 
     public void make () {
@@ -66,8 +68,11 @@ public abstract class Save {
         callback(StatusPassed.EMPTY_WORLD_CREATED);
     }
 
-
+    public int totalChunks = 0;
+    public int passedChunks = 0;
+    public int progress = 0;
     private void getModifications () {
+        passedChunks = 0;
         for (Chunk ch : chunks) {
             final Chunk al = emptyWorld.getChunkAt(ch.getX(), ch.getZ());
             ch.load();
@@ -123,13 +128,27 @@ public abstract class Save {
         file.set("seed", world.getSeed());
         file.set("chunks", changes);
 
-        callback(StatusPassed.SAVED);
+        passedChunks++;
+        progress = (int) Math.floor(passedChunks/chunks.length)*100;
+
+        callback(StatusPassed.SAVING);
     }
 
 
 
 
-    public enum StatusPassed {BLOCK_ID_INITIALISED, EMPTY_WORLD_CREATED, SAVED }
+    public enum StatusPassed {BLOCK_ID_INITIALISED, EMPTY_WORLD_CREATED, SAVING }
     public abstract void callback (StatusPassed status);
+
+    Date lastCallBack = new Date();
+    public boolean cooldown (int seconds) {
+        Date now = new Date();
+        if (progress == 100 || lastCallBack.getTime() + seconds*1000 > now.getTime()) {
+            lastCallBack = now;
+            return true;
+        } else return false;
+    }
+
+
 
 }
